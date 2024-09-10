c9b76738a559b8632cbbcc3dfd80a8b16084bd27
aem2231 <mariojetullaji@gmail.com>
2024-09-10 12:16:19 +0100

    idek what to put here

---
+++ rockpaperscissors.py
@@ -0,0 +1,34 @@
+import random
+
+def printRules():
+    print("""The computer will think of rock, paper or scissors.
+Enter r for rock, s for scissors and p for paper.
+The computer will reveal its choice and the winner.""")
+
+def playGame():
+    choices = ["Rock","Paper","Scissors"]
+    playerChoice = input("Enter r for rock, s for scissors and p for paper: ")
+    computerChoice = choices[random.randint(0,2)]
+    print(f'The computer chose {computerChoice}')
+    computerChoice = computerChoice[1].lower()
+
+    if computerChoice == playerChoice:
+        print('Its a draw!')
+    elif computerChoice == 'r' and playerChoice.lower() == 'p':
+        print('Player wins!')
+    elif computerChoice == 'p' and playerChoice.lower() == 's':
+        print('Player wins!')
+    elif computerChoice == 's' and playerChoice.lower() == 'r':
+        print('Player wins!')
+    else:
+        print('Computer wins!')
+
+def main():
+    print('Welcome to the rock, paper, scissors game!')
+    printRules()
+    playGame()
+    input('Press enter to exit.')
+
+main()
+
+
