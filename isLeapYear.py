f40feb866e4af4a2fea29951673f1cf5268dcd42
aem2231 <mariojetullaji@gmail.com>
2024-09-17 11:31:42 +0100

    added try except block

--- isLeapYear.py
+++ isLeapYear.py
@@ -1,16 +1,20 @@
 def main():
     isLeapYear = False
-    year = int(input('Enter a year: '))
+    try:
+        year = int(input('Enter a year: '))
+        if year == 0:
+            isLeapYear = True
 
-    if year == 0:
-        isLeapYear = True
+        if (year%4 == 0 and year%100 != 0) or (year%100 == 0 and year%400 == 0):
+            isLeapYear = True
 
-    if (year%4 == 0 and year%100 != 0) or (year%100 == 0 and year%400 == 0):
-        isLeapYear = True
+        if isLeapYear:
+            print(f'{year} is a leap year.')
+        else:
+            print(f'{year} is not a leap year.')
 
-    if isLeapYear:
-        print(f'{year} is a leap year.')
-    else:
-        print(f'{year} is not a leap year.')
+    except ValueError:
+        print('Enter a valid integer.\n')
+        main()
 
 main()
