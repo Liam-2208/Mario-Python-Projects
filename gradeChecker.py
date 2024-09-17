b4d050b1b5476d0df717fa8f3a246e107ca749e3
aem2231 <mariojetullaji@gmail.com>
2024-09-17 11:23:11 +0100

    idek anymore

--- gradeChecker.py
+++ gradeChecker.py
@@ -1,30 +1,36 @@
 def main():
-    try:
-        score = int(input('Enter your score [0-100]: '))
-    except ValueError:
-        print('Enter a valid number.')
-        main()
-        
-    if score > 100 or score < 0:
-        print('Invalid input.\n')
-        main()
+    try:    
+        score = int(input('Enter your score [1-100]: '))
+        if score > 100 or score < 0:
+            print('Enter a valid number.\n')
+            main()
 
-    if score >= 90:
-        grade = 'A*'
-    elif score >= 80:
-        grade = 'A'
-    elif score >=70:
-        grade = 'B'
-    elif score >=50:
-        grade = 'C'
-    elif score >=30:
-        grade = 'D'
-    else:
-        grade = 'E'
+        if score >= 90:
+            grade = 'A*'
+            pass
+        elif score >= 80:
+            grade = 'A'
+            pass
+        elif score >=70:
+            grade = 'B'
+            pass
+        elif score >=50:
+            grade = 'C'
+            pass
+        elif score >=30:
+            grade = 'D'
+            pass
+        elif score < 30:
+            grade = 'E'
+            pass
 
-    if score>=50:
-        print(f'You passed.\nGrade: {grade}')
-    else:
-        print(f'You failed.\nGrade: {grade}')
+        if score>=50:
+            print(f'You passed.\nGrade: {grade}')
+        elif score<50:
+            print(f'You failed.\nGrade: {grade}')
+    
+    except (ValueError, TypeError):
+        print('Enter a valid number.\n')
+        main()
 
 main()
