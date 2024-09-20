cf3c39fbff768ba6cf5369c9c45e132dce59a38c
aem2231 <mariojetullaji@gmail.com>
2024-09-20 12:24:14 +0100

    vending machine ig

---
+++ vendingMachine.py
@@ -0,0 +1,10 @@
+def main():
+    items = {
+        "A1": ["Original", 1.55],
+        "A2": ["Ultra White", 1.45],
+        "B1": ["Ultra Rosa", 1.50],
+        "B2": ["Bad Apple", 1.60],
+        "C1": ["Orange Dreamsicle", 1.60],
+        "C2": ["Ultra Strawbery Dreams", 1.65],
+        "D1": ["Ultra Paradise", 1.45]
+    }
