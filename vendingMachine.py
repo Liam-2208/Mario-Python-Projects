204021911dfaae382311718448e8642eedc8db89
aem2231 <mariojetullaji@gmail.com>
2024-09-20 16:13:54 +0100

    finished i think

--- vendingMachine.py
+++ vendingMachine.py
@@ -1,5 +1,23 @@
+import math
+
+def payment(credit, valid_coins, items, choice):
+    while credit <= items[choice][1]:
+        coin = float(input(f"Amount due: {round((items[choice][1] - credit), 2)}\nCoins Accepted: {valid_coins}\nInsert Coins: "))
+        if coin not in valid_coins:
+            print("Coin not accepted.")
+        else:
+            credit+=coin
+
+    if credit > items[choice][1]:
+        print(f"You have {round((credit - items[choice][1]), 2)} in change")
+        print(f"{items[choice][0]} has been dispensed.")
+    else:
+        print(f"{items[choice][0]} has been dispensed.")
+
+
 def main():
     credit = 0
+    choice = ""
     valid_coins = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 2]
     items = {
         "A1": ["Original", 1.55],
@@ -12,9 +30,13 @@ def main():
         "D2": ["Khaotic", 1.50]
     }
 
+    print("Items:")
     for key in items:
         print(f"{key}: {items[key]}")
 
-    coin = input(f"Please insert a coin.\nCoins accepted: {valid_coins}")
+    choice = input("Please pick an item: ")
+    payment(credit, valid_coins, items, choice)
+    main()
+
 
 main()
