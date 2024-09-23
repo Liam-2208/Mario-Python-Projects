98f9599374e5517ac02586c027b8c35c0d4b1e4d
aem2231 <mariojetullaji@gmail.com>
2024-09-23 15:32:37 +0100

    refactored and renamed variables

--- vendingMachine.py
+++ vendingMachine.py
@@ -1,30 +1,53 @@
 import time
 
-def payment(credit, valid_coins, items, choice):
-    while credit < items[choice][1]:
+def dispense(change, item, credit):
+    if change > 0:
+        print(f"You have {change} in change")
+        print(f"{item} has been dispensed.")
+        time.sleep(1)
+    else:
+        print(f"You have no chnage.")
+        print(f"{item} has been dispensed.")
+        time.sleep(1)
+
+def payment(credit, items, selected_item):
+    valid_coins = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 2]
+    amount_due = items[selected_item][1]
+    item = items[selected_item][0]
+    while credit < amount_due:
         try:
-            print(f"\nItem selected: {items[choice][0]}")
-            coin = float(input(f"Amount due: {round((items[choice][1] - credit), 2)}\nCoins Accepted: {valid_coins}\nInsert Coins: "))
+            print(f"\nItem selected: {selected_item}")
+            amount_due = items[selected_item][1] - credit
+            coin = float(input(f"Amount due: {round(amount_due, 2)}\nCoins Accepted: {valid_coins}\nInsert Coins: "))
             if coin not in valid_coins:
                 print("Coin not accepted.")
-                time.sleep(2)
+                time.sleep(1)
             else:
-                credit+=coin
+                credit += coin
         except ValueError:
             print("Enter coin as a float.\n")
             time.sleep(1)
 
-    if credit > items[choice][1]:
-        print(f"You have {round((credit - items[choice][1]), 2)} in change")
-        print(f"{items[choice][0]} has been dispensed.")
-    else:
-        print(f"{items[choice][0]} has been dispensed.")
+    change = credit - items[selected_item][1]
+    return change, credit
 
+def select_item(credit, items):
+    print("Items:")
+    for key in items:
+        print(f"{key}: {items[key]}")
+    selected_item = input("Please pick an item: ")
+    selected_item = selected_item.capitalize()
+    while selected_item not in items:
+        print("Please choose a valid choice.\n")
+        selected_item = input("\nPlease pick an item: ")
+        for key in items:
+            print(f"{key}: {items[key]}")
+        time.sleep(1)
+    return selected_item
 
 def main():
     credit = 0
     choice = ""
-    valid_coins = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 2]
     items = {
         "A1": ["Original", 1.55],
         "A2": ["Ultra White", 1.45],
@@ -36,17 +59,9 @@ def main():
         "D2": ["Khaotic", 1.50]
     }
 
-    print("Items:")
-    for key in items:
-        print(f"{key}: {items[key]}")
-    choice = input("Please pick an item: ")
-    choice = choice.capitalize()
-    if choice not in items:
-        print("Please choose a valid choice.\n")
-        time.sleep(1)
-        main()
-    payment(credit, valid_coins, items, choice)
-    main()
-
+    selected_item = select_item(credit, items)
+    change, credit = payment(credit, items, selected_item)
+    item = items[selected_item][0]
+    dispense(change, item, credit)
 
 main()
