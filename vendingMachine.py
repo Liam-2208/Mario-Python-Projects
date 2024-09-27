364819e81530c3f4dabb64dfc1b141d6d65be823
aem2231 <mariojetullaji@gmail.com>
2024-09-27 11:11:09 +0100

    fixed conflicts

--- vendingMachine.py
+++ vendingMachine.py
@@ -2,7 +2,7 @@ import time
 
 def dispense(item, credit):
     if credit > 0:
-        print(f"You have {credit} in change")
+        print(f"You have {round(credit, 2)} in change")
         print(f"{item} has been dispensed.")
         time.sleep(1)
     else:
@@ -10,7 +10,8 @@ def dispense(item, credit):
         print(f"{item} has been dispensed.")
         time.sleep(1)
 
-def payment(credit, items, selected_item):
+def payment(items, selected_item):
+    credit = 0
     valid_coins = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 2]
     amount_due = items[selected_item][1]
     item = items[selected_item][0]
@@ -39,14 +40,14 @@ def select_item(items):
     selected_item = selected_item.capitalize()
     while selected_item not in items:
         print("Please choose a valid choice.\n")
-        selected_item = input("\nPlease pick an item: ")
+        time.sleep(1)
         for key in items:
             print(f"{key}: {items[key]}")
-        time.sleep(1)
+        selected_item = input("\nPlease pick an item: ")
+        selected_item = selected_item.capitalize()
     return selected_item
 
 def main():
-    credit = 0
     choice = ""
     items = {
         "A1": ["Original", 1.55],
@@ -55,5 +56,11 @@ def main():
         "B2": ["Bad Apple", 1.60],
         "C1": ["Orange Dreamsicle", 1.60],
         "C2": ["Ultra Strawbery Dreams", 1.65],
-        "D1": ["Ultra Paradise", 1.45]
+        "D1": ["Ultra Paradise", 1.45],
+        "D2": ["Khaotic", 1.55]
     }
+    item = select_item(items)
+    credit = payment(items, item)
+    dispense(item, credit)
+
+main()
