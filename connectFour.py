bdfdc6c98e381a322e07c56915a61f6bea483b3d
aem2231 <mariojetullaji@gmail.com>
2024-10-06 17:36:38 +0100

    enhanced input validation with improved feedback, improved variable names, added checks for full columns

--- connectFour.py
+++ connectFour.py
@@ -1,11 +1,14 @@
 import os
 import time
 
+WIN_LENGTH = 4
+COLUMNS = 6
+ROWS = 5
+
 def draw(grid):
-    i = 0
     print("")
-    print("0 1 2 3 4 5 6")
-    print("| | | | | | |")
+    print(" ".join(str(i) for i in range(COLUMNS)))
+    print("|" + " |" * (int(COLUMNS)-1))
     for row in grid:
         print(f"{' '.join(row)}")
 
@@ -18,27 +21,29 @@ def update_grid(grid, column, row, player):
     grid[row][column] = piece
     return grid
 
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def check_horizontal(grid):
     for row in grid:
-        for i in range(len(row)-3):
-            temp = "".join(row[i:i+4])
+        for row_index in range(ROWS-3):
+            temp = "".join(row[row_index:row_index+WIN_LENGTH])
 
             if temp == "BBBB":
                 return True
             elif temp == "RRRR":
                 return True
-            i+=1 
+            row_index+=1 
     return False
 
 def check_vertical(grid):
     column = ""
-    for column_top in range(0, len(grid[0])):
-        for row in range(0, len(grid)):
+    for column_top in range(COLUMNS):
+        for row in range(ROWS):
             column+=grid[row][column_top]
 
-        for i in range(0, len(column)-3):
-            temp = column[i:i+4]
+        for column_index in range(COLUMNS-3):
+            temp = column[column_index:column_index+WIN_LENGTH]
             print(temp)
             if temp == "BBBB":
                 return True
@@ -53,7 +58,7 @@ def check_diagonal(grid, player):
     counter = "B" if player == 1 else "R"
 
     for row in range(len(grid)):
-        for column in range(len(grid[0])):
+        for column in range(COLUMNS):
             if grid[row][column] != counter:
                 continue
 
@@ -62,7 +67,7 @@ def check_diagonal(grid, player):
                 top_left_to_bottom_right[key1] = 0
             top_left_to_bottom_right[key1] += 1
             
-            if top_left_to_bottom_right[key1] >= 4:
+            if top_left_to_bottom_right[key1] >= WIN_LENGTH:
                 return True
 
             key2 = column + row
@@ -70,11 +75,10 @@ def check_diagonal(grid, player):
                 bottom_left_to_top_right[key2] = 0
             bottom_left_to_top_right[key2] += 1
             
-            if bottom_left_to_top_right[key2] >= 4:
+            if bottom_left_to_top_right[key2] >= WIN_LENGTH:
                 return True
 
     return False
-
 
 def check_wins(grid, player, turns, board):
     won = check_diagonal(grid, player) or check_vertical(grid) or check_horizontal(grid)
@@ -84,50 +88,64 @@ def check_wins(grid, player, turns, board):
         return "All spaces filled. Its a draw!!"
 
 def main():
-    board = [["0","0","0","0","0","0","0"] for i in range(0,6)]
+    board = [["0" for _ in range(COLUMNS)] for _ in range(ROWS)]
     won = False
     player = 1
     turns = 1
-    c_choice = 0
+    column_choice = 0
     lowest_point = 0
 
     while not won == True:
         draw(board)
        
-        if turns == len(board)*len(board[0]):
+        if turns == COLUMNS*ROWS:
             print("All spaces filled!\nIt's a draw!.")
 
         print(f"It is player {player}'s go.")
 
-        c_choice = int(input("Pick a column: "))
         while True:
+            column_choice = input("Pick a column: ")
+            
             try:
-                if c_choice not in range(0, len(board[0])):
-                    print("Column outside range.")
-                    c_choice = int(input("Pick a column: "))
+                column_choice = int(column_choice)
+                
+                if column_choice < 0 or column_choice >= COLUMNS:
+                    print(f"Column outside range. Please pick a column between 0 and {COLUMNS - 1}.")
                 else:
                     break
             except ValueError:
-                print("Column should be a valid number between 0 and 6.")
-        
-        lowest_point = len(board) - 1
-        for row in reversed(board):
-            if row[c_choice] != "0":
-                lowest_point = board.index(row) -1
+                print("Input must be a valid number.")
+            
+            time.sleep(1)
+            clear_console()
+            draw(board)
 
-        board = update_grid(board, c_choice, lowest_point, player)
+        lowest_point = len(board) - 1
+        for row in reversed(range(ROWS)):
+            if row[column_choice] != "0":
+                lowest_point = row
+
+        if lowest_point == -1:
+            print(f"Column {column_choice} is full. Please choose another.")
+        else:
+            board = update_grid(board, column_choice, lowest_point, player)
+
         won = check_wins(board, player, turns, board)
 
         if won == True:
             winner = player
+        elif won == type(str):
+            print(won)
+            break
 
-        os.system('cls' if os.name == 'nt' else 'clear')
+        clear_console()
         if player == 1:
             player = 2
         else:
             player = 1
         turns+=1
-        
+    
+    draw(board)
     print(f"Player {winner} has won the game.")
 
 if __name__ == "__main__":
