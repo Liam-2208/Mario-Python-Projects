2809b81187d8478cb10599ae4c8555f70e8f5e63
aem2231 <mariojetullaji@gmail.com>
2024-11-08 14:54:11 +0000

    started new assignment, moved assignments to assignments folder

---
+++ Assignments/coinCount/main.py
@@ -0,0 +1,33 @@
+import os
+import math
+
+#def display_bags() -> None:
+    #code goes here
+
+#def calculate_weight() -> int:
+    #code goes here
+
+def load_data():
+    coin_data: dict[str, list[str, float, bool]] = {}
+    if not os.path.exists("./coinCount.txt"): # Check if file exists
+        os.system('' if os.name == 'nt' else 'touch coinCount.txt')  # If file does not exist, create it
+        with open("coinCount.txt", "w") as data:
+            data.write(f"Volunteer{SPACING*' '}Coin{SPACING*' '}Bag_Weight{SPACING*' '}Correct") # Write default data to file
+    else:
+        with open("coinCount.txt", "r") as data: # If file does, exist, dump data into a dictionary
+            next(data) # Skip header line
+            for line in data:
+                arr = line.split(" "*SPACING) # Split line by removing spa
+                print(arr)     
+                key: string = arr[0] # Set volunteers name as key
+                if key not in coin_data: 
+                    coin_data[key] = arr[1:4]
+
+def main() -> None:
+    SPACING = 7
+    coin_data: dict[str, list[str, float, bool]] = load_data()
+
+
+    print(coin_data)    
+            
+main()
