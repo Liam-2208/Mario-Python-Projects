dbd52ebb34456875fa78f4756816d3ea4a71e752
aem2231 <mariojetullaji@gmail.com>
2024-11-12 11:05:17 +0000

    idek

--- Assignments/coinCount/main.py
+++ Assignments/coinCount/main.py
@@ -1,34 +1,56 @@
 import os
 import math
+import json
+import time
 
-#def display_bags() -> None:
-    #code goes here
+def clear_console():
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear') 
 
-#def calculate_weight() -> int:
-    #code goes here
-
-def load_data():
-    coin_data: dict[str, list[str, float, bool]] = {}
-    if not os.path.exists("./coinCount.txt"): # Check if file exists
-        os.system('' if os.name == 'nt' else 'touch coinCount.txt')  # If file does not exist, create it
-        with open("coinCount.txt", "w") as data:
-            data.write(f"Volunteer{SPACING*' '}Coin{SPACING*' '}Bag_Weight{SPACING*' '}Correct") # Write default data to file
-    else:
-        with open("coinCount.txt", "r") as data: # If file does, exist, dump data into a dictionary
-            next(data) # Skip header line
-            for line in data:
-                arr = line.split(" "*SPACING) # Split line by removing spa
-                print(arr)     
-                key: string = arr[0] # Set volunteers name as key
-                if key not in coin_data: 
-                    coin_data[key] = arr[1:4]
-
-def update_data():
+def load_data(): #Writes contents of txt to JSON
+    with open("coinCount.txt", "r") as data_txt: 
+        with open("data.json", "w") as data_json: 
+            for line in data_txt:
+                data_json.writelines(line)
     
 
 def main() -> None:
-    SPACING = 7 # Amount of spacing between elements in .txt file
-    coin_data: dict[str, list[str, float, bool]] = load_data()
+    load_data()
+    valid_coins = [2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.01]
+
+    name: str = input("Enter name: ")
+    try:
+        coin: float = float(input(f"Valid Coins: {valid_coins}\nEnter coin: "))
+        while coin not in valid_coins:
+            print("Coin not valid.")
+            clear_console()
+            coin: float = float(input(f"Valid Coins: {valid_coins}\nEnter coin: "))
+    except (TypeError, ValueError):
+        print("Coin should be of type float, eg: £2 = 2.0")
+        clear_console()
+    
+    while True:
+        try:
+            weight: int = int(input("Enter weight: "))
+            break
+        except (TypeError, ValueError):
+            print("Value should be of type int.")
+            clear_console()
+
+    
+    coin_data: dict[str, dict[str, float, int, bool]] = {
+        f"{coin}":
+            {
+            "name": name,
+            "coin": coin,
+            "weight": weight,
+            "is_correct": False
+            }
+        }
+    
+    with open("data.json", "w") as data_json:
+        data_json.writelines(json.dumps(coin_data, indent = 4))
+
 
 
 
