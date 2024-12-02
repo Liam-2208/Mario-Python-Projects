0b2f84b22bfeb232be4e2ca14f2dbf8bc33ab7a4
aem2231 <mariojetullaji@gmail.com>
2024-12-02 14:09:38 +0000

    i hate merge conflicts

--- Assignments/coinCount/main.py
+++ Assignments/coinCount/main.py
@@ -8,26 +8,28 @@ import math
     #code goes here
 
 def load_data():
-    coin_data: dict[str, list[str, float, bool]] = {}
+    SPACING = 7
+
+    coin_data: dict[str, list[str]] = {}
     if not os.path.exists("./coinCount.txt"): # Check if file exists
         os.system('' if os.name == 'nt' else 'touch coinCount.txt')  # If file does not exist, create it
         with open("coinCount.txt", "w") as data:
-            data.write(f"Volunteer{SPACING*' '}Coin{SPACING*' '}Bag_Weight{SPACING*' '}Correct") # Write default data to file
+            data.write(f"Volunteer{SPACING*' '}Coin{SPACING*' '}Bag_Weight{SPACING*' '}Is_Correct") # Write default data to file
     else:
-        with open("coinCount.txt", "r") as data: # If file does, exist, dump data into a dictionary
+        with open("coinCount.txt", "r") as data: # If file does exist, dump data into a dictionary
             next(data) # Skip header line
             for line in data:
-                arr = line.split(" "*SPACING) # Split line by removing spa
+                arr = line.split(" "*SPACING) # Split line by removing spaces
                 print(arr)     
                 key: string = arr[0] # Set volunteers name as key
                 if key not in coin_data: 
                     coin_data[key] = arr[1:4]
+    return coin_data #key[name] = coin, key[1] = weight, key[2] = is_correct
 
 def main() -> None:
-    SPACING = 7
-    coin_data: dict[str, list[str, float, bool]] = load_data()
+    coin_data: dict[str, list[str]] = load_data()
 
 
-    print(coin_data)    
+    print(coin_data[])    
             
 main()
