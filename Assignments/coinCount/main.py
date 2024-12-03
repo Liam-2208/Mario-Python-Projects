4fe1de1aa53dbf94e3f20917293d7f7960478fcf
aem2231 <mariojetullaji@gmail.com>
2024-12-03 10:30:03 +0000

    idk cro

--- Assignments/coinCount/main.py
+++ Assignments/coinCount/main.py
@@ -8,7 +8,7 @@ import time
 import json
 
 root_dir: Path = Path(os.getcwd())
-data_file_path: Union[str, Path] = Path(root_dir / "data.txt")
+data_file_path: Union[str, Path] = Path(root_dir / "data.txt" )
 
 bag_data: dict[str: list[str, float]] = {
     #Coin  amount  coin weight
@@ -68,15 +68,21 @@ def check_bag() -> bool:
     coin_value: float = coin_data[coin] # Gets the value of the coin, eg 2.0 or 0.5
     coin_weight: float = bag_data[coin][1] # Gets the coins weight, eg 12.00
     expected_value: float = coin_data[bag_data[coin][0]]
-    expected_weight: float = expected_value / coin_weight
+    expected_weight: float = (expected_value / coin_value) * coin_weight
     expected_coin_quantity: int = expected_value / coin_value
+    print(expected_weight)
+    time.sleep(1)
+    weight: float = 0
 
-    try:
-        weight: float = float(input("Enter weight "))
-        coin_quantity: int = round(weight / coin_weight)
-        while weight != expected_weight:
+        
+    while weight != expected_weight:
+        try:
+            weight: float = float(input("Enter weight "))
+            coin_quantity: int = round(weight / coin_weight)
             coin_offset: int = expected_coin_quantity - coin_quantity
-            print(f"Incorrect weight!\n")
+            print(coin_offset)
+            time.sleep(1)
+
             if coin_offset > 0:
                 print(f"Add ≈{coin_offset} more coins.")
                 os.system('cls' if os.name == 'nt' else 'clear')
@@ -84,8 +90,8 @@ def check_bag() -> bool:
                 print(f"Remove ≈{abs(coin_offset)} to continue.")
             weight: float = float(input("Enter weight "))
 
-    except (TypeError, ValueError) as e:
-        print(f"Value must be of type int: {e}")
+        except (TypeError, ValueError) as e:
+            print(f"Value must be of type int: {e}")
 
 def view_total() -> None:
     pass
@@ -96,7 +102,8 @@ def list_volunteers() -> None:
 def main() -> None:
     # Check if data file exists
     if not data_file_path.is_file():
-        with open(data_file_path, "w"):
+        with open(data_file_path, "w") as f:
+            f.write(json.dumps([]))  # Create an empty list if the file doesn't exist
             print("Data file created.")
             os.system('cls' if os.name == 'nt' else 'clear')
 
--- pandas/main.py
+++ pandas/main.py
@@ -1,22 +1,69 @@
 import pandas as pd
-
-# Creating a sample DataFrame with student information
-data = {'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Alice', 'Bob', 'Charlie'],
-        'Subject': ['Math', 'Math', 'Math', 'Math', 'English', 'English', 'English'],
-        'Score': [80, 90, 75, 85, 85, 88, 92],
-        'Attendance': [90, 95, 80, 92, 88, 93, 78]}
+from pymenu import select_menu
+import time
+import os
 
 
-df = pd.DataFrame(data)
+# Outputs the initial menu and checks validates the input
+def main_menu():
+    options: list[str] = ["Total income by source"]
+    choice: str = ""
 
-students = ['Alice', 'Bob', 'Charlie', 'David']
-student_avg_grades = df.groupby('Name')['Score'].mean().sort_values(ascending=True)
-print(round(student_avg_grades.iloc[-1], 2))
+    while choice not in options:
+        try:
+            choice: str = select_menu.create_select_menu(options, "Recoats Adventure Park\nSelect an option: ")
+        except (IndexError, TypeError, ValueError) as e:
+            print(f"Invalid option: {e}")
+            time.sleep(1)
+            os.system('cls' if os.name == 'nt' else 'clear')
 
+    return options.index(choice)
 
+# Submenu for totals, provides type check validation for the input
+def total_menu ():
+    options: list[str] = ["Tickets", "Gift Shop", "Snack Stand", "Pictures"]
+    choice: str = ""
+    
+    while choice not in options:
+        try:
+            choice: str = select_menu.create_select_menu(options, "Total Income By Source\nSelect an option:")
+        except (IndexError, TypeError, ValueError) as e:
+            print(f"Invlaid options: {e}")
+            time.sleep(1)
+            os.system('cls' if os.name == 'nt' else 'clear')
 
+def payment_type_trends() -> None:
+    pass
 
-# Displaying the DataFrame
-print("Original DataFrame:")
-print(df)
-print("\n")
+# takes the total submenu input and converts the number to a string of the source name
+def convert_total_menu_choice(total_men_choice):
+    
+    if total_men_choice == "1":
+        total_choice = "Tickets"
+    elif total_men_choice == "2":
+        total_choice = "Gift Shop"
+    elif total_men_choice == "3":
+        total_choice= "Snack Stand"
+    else:
+        total_choice = "Pictures"  
+    
+    return total_choice
+
+# creates a new dataframe with the selected income source then creates a total row
+# outputs the final total in a message
+def get_total_data(total_choice):
+    
+    df = pd.read_csv("Task4a_data.csv")
+    
+    income = df[["Day", total_choice]]
+
+    total = income[total_choice].sum()
+
+    msg = "The total income from {} was: £{}".format(total_choice, total)
+    return msg
+
+main_menu_choice = main_menu()
+if main_menu_choice == 0:
+    total_men_choice = total_menu()
+    total_choice = convert_total_menu_choice(total_men_choice)
+    print(get_total_data(total_choice))
