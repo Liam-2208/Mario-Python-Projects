ff80f4983e7828fb4053be1176dd4f9463e75e2c
aem2231 <mariojetullaji@gmail.com>
2024-12-03 12:44:05 +0000

    some data anlaysis stuff

--- pandas/main.py
+++ pandas/main.py
@@ -2,11 +2,12 @@ import pandas as pd
 from pymenu import select_menu
 import time
 import os
+import matplotlib.pyplot as plt
 
 
 # Outputs the initial menu and checks validates the input
 def main_menu():
-    options: list[str] = ["Total income by source"]
+    options: list[str] = ["Total income by source", "Income Sources By Day"]
     choice: str = ""
 
     while choice not in options:
@@ -32,17 +33,32 @@ def total_menu ():
             time.sleep(1)
             os.system('cls' if os.name == 'nt' else 'clear')
 
-def payment_type_trends() -> None:
-    pass
+def income_sources_by_day(source: str) -> None:
+    sorted_weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    df = pd.read_csv('Task4a_data.csv')
+    income_sources_by_day = df.groupby("Day")[source].sum()
+    income_sources_by_day.index = pd.Categorical(income_sources_by_day.index, categories=sorted_weekdays, ordered=True)
+    income_sources_by_day = income_sources_by_day.sort_index()
+    days = sorted_weekdays
+    income = income_sources_by_day.values.tolist()
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(days, income)
+    plt.title(f"Income from {source} by Day")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
 
 # takes the total submenu input and converts the number to a string of the source name
-def convert_total_menu_choice(total_men_choice):
+def convert_total_menu_choice(total_menu_choice):
     
-    if total_men_choice == "1":
+    if total_menu_choice == "1":
         total_choice = "Tickets"
-    elif total_men_choice == "2":
+    elif total_menu_choice == "2":
         total_choice = "Gift Shop"
-    elif total_men_choice == "3":
+    elif total_menu_choice == "3":
         total_choice= "Snack Stand"
     else:
         total_choice = "Pictures"  
@@ -64,6 +80,18 @@ def get_total_data(total_choice):
 
 main_menu_choice = main_menu()
 if main_menu_choice == 0:
-    total_men_choice = total_menu()
-    total_choice = convert_total_menu_choice(total_men_choice)
+    total_menu_choice = total_menu()
+    total_choice = convert_total_menu_choice(total_menu_choice)
     print(get_total_data(total_choice))
+elif main_menu_choice == 1:
+    options: list[str] = ["Tickets", "Gift Shop", "Snack Stand", "Pictures"]
+    source = ""
+    while source not in options:
+        try:
+            source = select_menu.create_select_menu(options, "Select an income source:")
+        except (IndexError, TypeError, ValueError) as e:
+            print(f"Invalid option: {e}")
+            time.sleep(1)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            
+    income_sources_by_day(source)
